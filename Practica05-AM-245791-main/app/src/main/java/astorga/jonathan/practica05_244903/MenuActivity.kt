92ca148090c24e298ea03288ee732d4c319dedfe
JLMadero <madero26jose08@gmail.com>
2025-02-26 19:22:35 -0700

    a

---
+++ Practica05-AM-245791-main/app/src/main/java/astorga/jonathan/practica05_244903/MenuActivity.kt
@@ -0,0 +1,65 @@
+package astorga.jonathan.practica05_244903
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import astorga.jonathan.practica05_245791.R
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_menu)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        var btnAntojitos: Button = findViewById(R.id.btnAntojitos) as Button
+        var btnTortas: Button = findViewById(R.id.btnTortas) as Button
+        var btnEspecialidades: Button = findViewById(R.id.btnEspecialidades) as Button
+        var btnCombos: Button = findViewById(R.id.btnCombinations) as Button
+        var btnSopas: Button = findViewById(R.id.btnSopas) as Button
+        var btnBebidas: Button = findViewById(R.id.btnDinks) as Button
+
+        btnAntojitos.setOnClickListener{
+            var intent : Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType", "ANTOJITOS")
+            startActivity(intent)
+        }
+
+        btnTortas.setOnClickListener{
+            var intent : Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType", "TORTAS")
+            startActivity(intent)
+        }
+
+        btnEspecialidades.setOnClickListener{
+            var intent : Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType", "ESPECIALIDADES")
+            startActivity(intent)
+        }
+
+        btnCombos.setOnClickListener{
+            var intent : Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType", "COMBOS")
+            startActivity(intent)
+        }
+
+        btnSopas.setOnClickListener{
+            var intent : Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType", "SOPAS")
+            startActivity(intent)
+        }
+
+        btnBebidas.setOnClickListener{
+            var intent : Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType", "BEBIDAS")
+            startActivity(intent)
+        }
+    }
+}
---
+++ Practica05-AM-245791-main/app/src/main/java/astorga/jonathan/practica05_244903/Product.kt
@@ -0,0 +1,3 @@
+package astorga.jonathan.practica05_244903
+
+data class Product(var name:String, var image:Int, var description:String, var price:Double)
